9605e33dff36eb441893dd405dcd1dfa83b2bc9e
praveenroot03 <praveentamil331999@gmail.com>
2019-02-28 22:20:00 +0530

    login commit

--- app/src/main/java/com/project/sudo/MainActivity.java
+++ app/src/main/java/com/project/sudo/MainActivity.java
@@ -4,8 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +19,11 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseAuth auth;
+
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -21,6 +32,23 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_profile:
                     mTextMessage.setText(R.string.title_profile);
+                    auth = FirebaseAuth.getInstance();
+                    if (auth.getCurrentUser() != null) {
+                        // already signed in
+                        Intent intent = new Intent(getApplicationContext(), OrganisationActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "not signin", Toast.LENGTH_SHORT).show();
+
+                        startActivityForResult(
+                                AuthUI.getInstance()
+                                        .createSignInIntentBuilder()
+                                        .setAvailableProviders(Arrays.asList(
+                                                new AuthUI.IdpConfig.EmailBuilder().build()))
+                                        .build(),
+                                RC_SIGN_IN);
+
+                    }
                     return true;
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
@@ -33,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
             }
             return false;
         }
+
+
     };
 
     @Override
@@ -43,6 +73,32 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = findViewById(R.id.message);
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.getMenu().getItem(1).setChecked(true);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "Signed in" +
+                        "", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), OrganisationActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    return;
+                }
+
+            }
+        }
     }
 
 }
