9615fae4780466be00874eb64fdbd60ca875477b
mukulanand8 <mukulanand254@gmail.com>
2019-02-28 17:35:59 +0530

    Organisation Activity added

--- app/src/main/java/com/project/sudo/MainActivity.java
+++ app/src/main/java/com/project/sudo/MainActivity.java
@@ -1,15 +1,14 @@
 package com.project.sudo;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
+                    Intent intent = new Intent(getApplicationContext(), OrganisationActivity.class);
+                    startActivity(intent);
                     return true;
                 case R.id.navigation_bookmarks:
                     mTextMessage.setText(R.string.title_bookmarks);
@@ -39,8 +40,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
---
+++ app/src/main/java/com/project/sudo/OrganisationActivity.java
@@ -0,0 +1,30 @@
+package com.project.sudo;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class OrganisationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organisation);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
